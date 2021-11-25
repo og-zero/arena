1f24f72b6cef27c9ee9af479a670d6e49e1cf80c
Constantine Bateruk <c.bateruk@pin-up.team>
2021-11-25 23:05:38 +0200

    WIP

--- pkg/temperature/temperature.go
+++ pkg/temperature/temperature.go
@@ -1,23 +1,33 @@
 package temperature
 
+const (
+	defaultTemperature float32 = 0
+
+	Celsius = iota
+	Farenheit
+)
+
 type (
 	Temperature interface {
 		C() float64 // return temperature in Celsius
 		F() float64
 	}
 
-	celsius   float32
-	farenheit float32
+	celsius     float32
+	farenheit   float32
+	temperature float32
 )
 
-const ()
-
-func (c celsius) toFarenheit() farenheit {
-	// Formula: (0°C × 9/5) + 32 = 32°F
-	return (farenheit(c) * 9 / 5) + 32
+func New() temperature {
+	return temperature(defaultTemperature)
 }
 
-func (f farenheit) toCelsius() celsius {
+func (t temperature) toFarenheit() farenheit {
 	// Formula: (0°C × 9/5) + 32 = 32°F
-	return (celsius(f) - 32) * 5 / 9
+	return (farenheit(t) * 9 / 5) + 32
+}
+
+func (t temperature) toCelsius() celsius {
+	// Formula: (0°C × 9/5) + 32 = 32°F
+	return (celsius(t) - 32) * 5 / 9
 }
